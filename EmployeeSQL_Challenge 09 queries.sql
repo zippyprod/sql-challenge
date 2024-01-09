72e1f625875c9b4361031e92c62e7ec355323825
Jeffrey A. Morgan <146994261+zippyprod@users.noreply.github.com>
2024-01-09 16:54:19 -0500

    10-SQL_challenge
    Uploading sql Challenge 10

---
+++ EmployeeSQL_Challenge 09 queries.sql
@@ -0,0 +1,59 @@
+--1. List the employee number, last name, first name, sex, and salary for each employee.
+SELECT employees.emp_no AS Employee_Number, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+JOIN salaries ON employees.emp_no = salaries.emp_no
+LIMIT (5);
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees
+WHERE employees.hire_date > '1986-1-1'::date AND employees.hire_date < '1987-1-1'::date
+LIMIT (5);
+
+
+--NOT WORKING
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+JOIN departments ON dept_manager.dept_no = departments.dept_no
+JOIN employees ON dept_manager.emp_no = employees.emp_no;
+
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+LIMIT (20);
+
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT * FROM departments;
+-- The department number of Sales department is d007
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd007';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT * FROM departments;
+-- The department numbers of Sales and Development departments are d007 and d005
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_no = 'd005' or departments.dept_no = 'd007';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "Frequency count" DESC;
+
+
---
+++ EmployeeSQL_SCHEMA.sql
@@ -0,0 +1,66 @@
+
+DROP TABLE IF EXISTS employees
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+ 
+CREATE TABLE employees(
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR(5)  NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY(emp_no)
+	--FOREIGN KEY(emp_title_id)
+);
+
+SELECT * FROM employees
+
+CREATE TABLE departments(
+	dept_no VARCHAR(5) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
+);
+
+SELECT * FROM departments
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(5) NOT NULL REFERENCES departments(dept_no),
+	PRIMARY KEY(emp_no, dept_no)
+);
+
+SELECT * FROM dept_emp
+
+
+
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(5) NOT NULL REFERENCES departments(dept_no),
+	emp_no INT NOT NULL,
+	PRIMARY KEY(dept_no, emp_no)
+);
+
+SELECT * FROM dept_emp
+
+
+CREATE TABLE salaries(
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT
+);
+
+SELECT * FROM salaries
+
+
+CREATE TABLE titles(
+	title_id VARCHAR(5),
+	title VARCHAR(30),
+	PRIMARY KEY (title_id)
+	
+);
+
+SELECT * FROM titles
